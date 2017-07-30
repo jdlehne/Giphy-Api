19dfa733d2bfcd73148435473e3a2e39ae30f973
josh lehne <jdlehne@gmail.com>
2017-07-30 16:47:45 -0500

    animating gifs

--- assets/app.js
+++ assets/app.js
@@ -4,35 +4,43 @@
  function displayGiphy() {
 
      var giphy = $(this).attr("data-name");
-     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + giphy + "&limit=10&api_key=dc6zaTOxFJmzC";
+     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + giphy + "&limit=10&api_key=dc6zaTOxFJmzC";
 
      $.ajax({
          url: queryURL,
          method: 'GET'
      }).done(function(response) {
 
-         console.log(response); //----get object
-         console.log(response.data[0].images.downsized_medium.url); //----gets gif address we use for var imgURL
+         console.log(response); //----get object----//
+         console.log(response.data[0].images.downsized_medium.url); //---gets gif address for var animate
+         console.log(response.data[0].images.downsized_still.url); //----gets gif address we use for var still
          console.log(response.data[0].rating); //----------------------returns rating
 
          for (var i = 0; i < response.data[0].images.downsized_medium.url.length; i++) { //---this code goes off of the return limit of 20
-             //for (var i = 0; i < giphys.length; i++) { //-------this code will match the array length
-             //var ratingDiv = $("<div class='rating'>");
+
              var giphyDiv = $("<div class='newGiphy'>");
 
              var rating = $("<p>").html("Rating: " + response.data[i].rating);
-             // Retrieving the URL for the image
-             var imgURL = response.data[i].images.downsized_medium.url;
 
-             // Creating an element to hold the image
-             var image = $("<img>").attr("src", imgURL);
+             var animate = response.data[i].images.downsized_medium.url;
+
+             var still = response.data[i].images.downsized_still.url;
+
+             // Creating an element to hold the image--and add data attributes for animating and class
+             var image = $("<img>").attr("src", still);
+             image.attr('data-still', still);
+             image.attr('data-animate', animate);
+             image.attr('data-state', image.data('state') === 'still' ?
+                 'animate' :
+                 'still');
+             image.addClass("gif");
 
              // Appending the image
              giphyDiv.append(rating, image);
 
-             //$("#giphy-view").prepend(ratingDiv);
              // Putting the entire giphy above the previous giphys
              $("#giphy-view").prepend(giphyDiv);
+             event.preventDefault();
          }
      });
  }
@@ -46,7 +54,7 @@
      // Looping through the array of giphys
      for (var i = 0; i < giphys.length; i++) {
 
-         // Then dynamicaly generating buttons for each giphy in the array
+         // --generating buttons for each giphy in the array
          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
          var a = $("<button>");
          // Adding a class of giphy to our button
@@ -61,7 +69,6 @@
  }
 
  $("#add-giphy").on("click", function(event) {
-     displayGiphy();
 
      event.preventDefault();
 
@@ -77,8 +84,22 @@
  $(document).ready(function() {
 
      renderButtons();
+      $("#giphy-input").focus();
 
      $(document).on("click", ".giphy", displayGiphy);
 
-    
+
+     $(document).on("click", '.gif', function() {
+
+         var state = $(this).attr("data-state");
+
+         if (state === "still") {
+             $(this).attr("src", $(this).attr("data-animate"));
+             $(this).attr("data-state", "animate");
+         } else {
+             $(this).attr("src", $(this).attr("data-still"));
+             $(this).attr("data-state", "still");
+         }
+     });
+
  });
